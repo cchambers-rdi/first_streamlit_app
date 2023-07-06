9ee2f5d0080a750c853770b70f185c1d67addbdf
Christopher Chambers <cchambers@resdat.com>
2023-07-06 15:45:06 -0600

    Update streamlit_app.py
    Added more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('Test Page Please Ignore')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
